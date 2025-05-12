0c27fd8037447caa57db38ef153a11850da5f63c
Wencho8 <wenchoavalos8@gmail.com>
2025-05-12 14:26:41 -0300

    Not found page

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+            <p>This is not the page you are looking for.</p>
+        </div>
+    );
+}
+
+export default NotFound;
