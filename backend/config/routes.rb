80159c9470291d254cd83b44d4aab65b3f5c3fee
Wencho8 <wenchoavalos8@gmail.com>
2025-05-12 14:21:23 -0300

    Routes used

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [] do
+    collection do
+      post 'signup'
+      post 'login'
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :items, only: [:create, :update, :destroy, :index]
+  
+  resources :orders, only: [:create, :index, :show, :update] do
+    member do
+      post :complete
+      post :cancel
+    end
+  end
 end
